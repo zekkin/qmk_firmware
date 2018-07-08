b33e65324c1028fcbedca670b3c3f602b414b473
gold <harris@jun-kin.org>
2018-07-08 10:44:45 -0500

    Pushing Kyuu stuff

---
+++ keyboards/kyuu/keymaps/zekk/keymap.c
@@ -0,0 +1,50 @@
+#include "kyuu.h"
+
+enum {
+  TD_5_F5 = 0,
+  TD_6_F6,
+  TD_H_E
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_5_F5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
+  [TD_6_F6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_F6),
+  [TD_H_E] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[0] = KEYMAP(
+		KC_GESC, KC_1, KC_2, KC_3, KC_4, TD(TD_5_F5), TD(TD_6_F6), KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_DEL, 
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP, 
+		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, 
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT, KC_UP, TD(TD_H_E), 
+		KC_RCTL, KC_TRNS, KC_LGUI, 			KC_SPC, 			MO(1), 		KC_LEFT, KC_DOWN, KC_RGHT),
+
+	[1] = KEYMAP(
+		RESET, KC_F1, KC_F2, KC_F3, KC_F4, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, 
+		KC_TRNS, KC_TRNS, KC_TRNS, 			KC_TRNS, 			KC_TRNS, 	KC_MPRV, KC_TRNS, KC_MNXT)
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+	switch (id) {
+	}
+	return MACRO_NONE;
+}
+
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+}
